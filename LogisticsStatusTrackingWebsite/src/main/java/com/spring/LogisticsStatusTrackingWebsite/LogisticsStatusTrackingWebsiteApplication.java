774f27a2bb9942b060921bcf98f5df7ac60a44ac
scolley <scolley.huang@kkday.com>
2023-12-01 03:28:00 +0800

    create hello world api

---
+++ LogisticsStatusTrackingWebsite/src/main/java/com/spring/LogisticsStatusTrackingWebsite/LogisticsStatusTrackingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.spring.LogisticsStatusTrackingWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsStatusTrackingWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsStatusTrackingWebsiteApplication.class, args);
+	}
+
+}
---
+++ LogisticsStatusTrackingWebsite/src/test/java/com/spring/LogisticsStatusTrackingWebsite/LogisticsStatusTrackingWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.LogisticsStatusTrackingWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsStatusTrackingWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
