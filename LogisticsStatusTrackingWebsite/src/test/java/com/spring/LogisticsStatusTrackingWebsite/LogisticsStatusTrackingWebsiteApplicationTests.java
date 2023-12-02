adc12d33bb27543847d9d8150f83865bfd04e13b
scolley <scolley.huang@kkday.com>
2023-12-02 22:28:37 +0800

    add demo dockerfile

--- LogisticsStatusTrackingWebsite/src/test/java/com/spring/LogisticsStatusTrackingWebsite/LogisticsStatusTrackingWebsiteApplicationTests.java
+++ LogisticsStatusTrackingWebsite/src/test/java/com/spring/LogisticsStatusTrackingWebsite/LogisticsStatusTrackingWebsiteApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LogisticsStatusTrackingWebsiteApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
