17f98c1f75a847a6a369add962844e25bbe5790a
scolley <scolley.huang@kkday.com>
2023-12-07 17:12:03 +0800

    wip

--- LogisticsStatusTrackingWebsite/src/main/java/com/spring/LogisticsStatusTrackingWebsite/LogisticsStatusTrackingWebsiteApplication.java
+++ LogisticsStatusTrackingWebsite/src/main/java/com/spring/LogisticsStatusTrackingWebsite/LogisticsStatusTrackingWebsiteApplication.java
@@ -2,6 +2,7 @@ package com.spring.LogisticsStatusTrackingWebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -11,19 +12,6 @@ public class LogisticsStatusTrackingWebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogisticsStatusTrackingWebsiteApplication.class, args);
-
-		LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory();
-		connectionFactory.afterPropertiesSet();
-
-		RedisTemplate<String, String> template = new RedisTemplate<>();
-		template.setConnectionFactory(connectionFactory);
-		template.setDefaultSerializer(StringRedisSerializer.UTF_8);
-		template.afterPropertiesSet();
-
-		template.opsForValue().set("foo", "bar");
-		System.out.println(template.opsForValue().get("foo"));
-
-		connectionFactory.destroy();
 	}
 
 }
--- LogisticsStatusTrackingWebsite/src/main/java/com/spring/LogisticsStatusTrackingWebsite/config/RedisConfig.java
+++ LogisticsStatusTrackingWebsite/src/main/java/com/spring/LogisticsStatusTrackingWebsite/config/RedisConfig.java
@@ -1,32 +1,24 @@
 package com.spring.LogisticsStatusTrackingWebsite.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.io.Serializable;
+@Configuration
+class RedisConfig {
 
-//@Configuration
-//@AutoConfigureAfter(RedisAutoConfiguration.class)
-//@EnableCaching
-//public class RedisConfig {
-//
-//    @Bean
-//    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-//        return RedisCacheManager.create(connectionFactory);
-//    }
-//
-//}
+    @Bean
+    LettuceConnectionFactory connectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+}
