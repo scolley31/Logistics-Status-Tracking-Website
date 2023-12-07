9d9e4aff85e7e47be61adb779642093f278c9a6a
scolley <scolley.huang@kkday.com>
2023-12-08 03:01:47 +0800

    wip

--- LogisticsStatusTrackingWebsite/src/main/java/com/spring/LogisticsStatusTrackingWebsite/controller/LogisticsController.java
+++ LogisticsStatusTrackingWebsite/src/main/java/com/spring/LogisticsStatusTrackingWebsite/controller/LogisticsController.java
@@ -5,9 +5,7 @@ import com.spring.LogisticsStatusTrackingWebsite.domain.response.ApiResponse;
 import com.spring.LogisticsStatusTrackingWebsite.domain.response.ErrorResponse;
 import com.spring.LogisticsStatusTrackingWebsite.domain.response.LogisticsStatus;
 import com.spring.LogisticsStatusTrackingWebsite.service.LogisticsService;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +21,7 @@ public class LogisticsController {
 
     private final LogisticsService logisticsService;
     private final StringRedisTemplate stringRedisTemplate;
+
     public LogisticsController(LogisticsService logisticsService, StringRedisTemplate stringRedisTemplate) {
         this.logisticsService = logisticsService;
         this.stringRedisTemplate = stringRedisTemplate;
@@ -31,12 +30,12 @@ public class LogisticsController {
     @GetMapping("/query")
     public ResponseEntity<ApiResponse<LogisticsStatus>> queryLogisticsStatus(@RequestParam("sno") String logisticsNumber) {
         Optional<LogisticsStatus> logisticsStatus = logisticsService.queryLogisticsStatus(logisticsNumber);
-        stringRedisTemplate.opsForValue().set("scolley34", "222");
         return logisticsStatus.map(status -> ResponseEntity.ok(new ApiResponse<>("success", status))).orElseGet(() -> ResponseEntity.ok(new ApiResponse<>("", null, new ErrorResponse(200, "Logistics number not found"))));
     }
 
     @GetMapping("/fake")
     public ResponseEntity<ApiResponse<List<LogisticsStatus>>> createFakeLogisticsStatus(@RequestParam("num") String dataCounts) {
+        stringRedisTemplate.opsForValue().set("123", "111");
         List<LogisticsStatus> fakeLogisticsStatuses = logisticsService.createFakeLogisticsStatus(dataCounts);
         return ResponseEntity.ok(new ApiResponse<>("success", fakeLogisticsStatuses));
     }
